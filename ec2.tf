f2f6e6693c348e6c415051d9a628fd44f934c237
DEEPAK894 <74669196+DEEPAK894@users.noreply.github.com>
2023-12-01 15:22:38 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,48 @@
+# ec2.tf
+
+provider "aws" {
+  region = "us-east-1" # Update with your desired region
+  profile = "default"
+}
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-0230bd60aa48260c6" # Replace with your AMI ID
+  instance_type          = "t2.micro"     # Adjust instance type as needed
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  key_name               = aws_key_pair.ec2_key_pair.key_name
+  associate_public_ip_address = true
+
+  # Additional configurations can be added here
+  tags = {
+    Name = "PublicInstance"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y nginx",  # Correct package name
+      "sudo service nginx start",
+      "sudo echo '${tls_private_key.ssh_key.private_key_pem}' > /home/ec2-user/private_key.pem",
+      "sudo chmod 400 /home/ec2-user/private_key.pem"  # Start NGINX service
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = tls_private_key.ssh_key.private_key_pem
+      host = self.public_ip
+    }
+  }
+}
+
+resource "aws_instance" "private_instance" {
+  ami                    = "ami-0230bd60aa48260c6" # Replace with your AMI ID
+  instance_type          = "t2.micro"     # Adjust instance type as needed
+  subnet_id              = aws_subnet.private_subnet.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  key_name               = aws_key_pair.ec2_key_pair.key_name
+
+  # Additional configurations for private instance
+  tags = {
+    Name = "PrivateInstance"
+  }
+  user_data =  file("user.sh")
+}
---
+++ ssh_key.tf
@@ -0,0 +1,16 @@
+#ssh
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# Remove this block from ssh_key.tf
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "ec2-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "./private_key.pem"
+}
